404d002fe6ee405f0b1da3263c3a52b485461042
chinnatan <korn.chinnatan@gmail.com>
2019-10-11 22:37:45 +0700

    update dependencies and router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.css'
+import 'animate.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
