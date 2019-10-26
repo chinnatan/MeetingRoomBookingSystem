89687e56aae3c2abe4619c3e055a128c69022e72
chinnatan <korn.chinnatan@gmail.com>
2019-10-27 00:43:16 +0700

    เพิ่ม component booking

---
+++ frontend/src/components/Booking.vue
@@ -0,0 +1,493 @@
+<template>
+  <div>
+    <navbar></navbar>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-8">
+              <div class="row">
+                <div class="col-md-12">
+                  <svg
+                    id="floor1"
+                    class="booking-font"
+                    width="100%"
+                    height="100%"
+                    x="605"
+                    y="270"
+                    overflow="visible"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:se="http://svg-edit.googlecode.com"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 600 440"
+                    stroke="none"
+                    fill="#000000"
+                    stroke-width="1px"
+                    v-if="floor === 1"
+                  >
+                    <!-- Created with SVG-edit - http://svg-edit.googlecode.com/ -->
+                    <g style="pointer-events:all" stroke="none" fill="#000000" stroke-width="1px">
+                      <title
+                        style="pointer-events:inherit"
+                        stroke="none"
+                        fill="#000000"
+                        stroke-width="1px"
+                      >Floor 1</title>
+                      <rect
+                        fill=#aaffff
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="138"
+                        y="137"
+                        width="92"
+                        height="92"
+                        id="floor_1_service"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="138"
+                        y="237"
+                        width="92"
+                        height="92"
+                        id="floor_1_m04"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="138"
+                        y="337"
+                        width="92"
+                        height="92"
+                        id="floor_1_m03"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="437"
+                        y="137"
+                        width="92"
+                        height="92"
+                        id="floor_1_m23"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="437"
+                        y="237"
+                        width="92"
+                        height="92"
+                        id="floor_1_m22"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="437"
+                        y="337"
+                        width="92"
+                        height="92"
+                        id="floor_1_m21"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="290"
+                        y="250"
+                        width="45"
+                        height="45"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="floor_1_m14"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="342"
+                        y="250"
+                        width="45"
+                        height="45"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="floor_1_m18"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="290"
+                        y="300"
+                        width="45"
+                        height="45"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="floor_1_m13"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="342"
+                        y="300"
+                        width="45"
+                        height="45"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="floor_1_m17"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="290"
+                        y="350"
+                        width="45"
+                        height="45"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="floor_1_m12"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="342"
+                        y="350"
+                        width="45"
+                        height="45"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="floor_1_m16"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="138"
+                        y="37"
+                        width="92"
+                        height="92"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="floor_1_111"
+                      />
+                      <rect
+                        fill="#bfbfbf"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        x="37"
+                        y="37"
+                        width="92"
+                        height="92"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="floor_1_auditorium"
+                      />
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="184"
+                        y="220"
+                        id="txt_service"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                      >{{ txtFloor1.roomService }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="184"
+                        y="320"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m04"
+                      >{{ txtFloor1.roomM04 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="184"
+                        y="420"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m03"
+                      >{{ txtFloor1.roomM03 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="485"
+                        y="220"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m23"
+                      >{{ txtFloor1.roomM23 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="484"
+                        y="320"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m22"
+                      >{{ txtFloor1.roomM22 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="484"
+                        y="420"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m21"
+                        style="cursor: move;"
+                      >{{ txtFloor1.roomM21 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="184"
+                        y="120"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_111"
+                      >{{ txtFloor1.room111 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="83"
+                        y="120"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_auditorium"
+                        style="cursor: move;"
+                      >{{ txtFloor1.auditorium }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="313"
+                        y="290"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m14"
+                      >{{ txtFloor1.roomM14 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="313"
+                        y="390"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m12"
+                      >{{ txtFloor1.roomM12 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="313"
+                        y="340"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m13"
+                      >{{ txtFloor1.roomM13 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="364"
+                        y="290"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m18"
+                      >{{ txtFloor1.roomM18 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="364"
+                        y="390"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m16"
+                      >{{ txtFloor1.roomM16 }}</text>
+                      <text
+                        fill="#000000"
+                        stroke="#000000"
+                        stroke-width="0px"
+                        stroke-dasharray="null"
+                        stroke-linejoin="null"
+                        stroke-linecap="null"
+                        x="364"
+                        y="340"
+                        text-anchor="middle"
+                        xml:space="preserve"
+                        fill-opacity="1"
+                        stroke-opacity="1"
+                        id="txt_m17"
+                      >{{ txtFloor1.roomM17 }}</text>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="row mt-4">
+                <div class="col-md-12">
+                  <h3 class="booking-font text-left">{{ txtShowFloor }}</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+
+export default {
+  name: "Booking",
+  components: {
+    Navbar: Navbar
+  },
+  created() {
+    
+  },
+  data() {
+    return {
+      floor: 1,
+      txtShowFloor: "แสดงชั้น",
+      txtFloor1: {
+        auditorium: "Auditorium",
+        room111: "111",
+        roomService: "Service",
+        roomM03: "M03",
+        roomM04: "M04",
+        roomM12: "M12",
+        roomM13: "M13",
+        roomM14: "M14",
+        roomM16: "M16",
+        roomM17: "M17",
+        roomM18: "M18",
+        roomM21: "M21",
+        roomM22: "M22",
+        roomM23: "M23"
+      },
+      colortest: "#aaffff"
+    };
+  },
+  mounted() {
+    document.getElementById('floor_1_service').setAttribute('fill', '#FFFFFF');
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+.booking-font {
+    font-family: "Mitr-Medium";
+    -webkit-touch-callout: none;
+    /* iOS Safari */
+    -webkit-user-select: none;
+    /* Safari */
+    -khtml-user-select: none;
+    /* Konqueror HTML */
+    -moz-user-select: none;
+    /* Old versions of Firefox */
+    -ms-user-select: none;
+    /* Internet Explorer/Edge */
+    user-select: none;
+    /* Non-prefixed version, currently supported by Chrome, Opera and Firefox */
+}
+</style>
