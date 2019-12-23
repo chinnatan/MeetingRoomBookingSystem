67ad6dbd096cfc2733dd5f73f36b4564e2e94a65
chinnatan <korn.chinnatan@gmail.com>
2019-12-23 15:00:48 +0700

    เพิ่ม vue router และ vue sweetalert สำหรับทำ popup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueSweetalert2 from 'vue-sweetalert2';
 import App from './App'
 import router from './router'
 
@@ -8,8 +9,10 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.css'
 import 'animate.css'
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.config.productionTip = false
+Vue.use(VueSweetalert2);
 
 /* eslint-disable no-new */
 new Vue({
