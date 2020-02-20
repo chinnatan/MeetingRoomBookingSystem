0291ed3aafcc54e9f350707a9e08b168645d1b8e
chinnatan <korn.chinnatan@gmail.com>
2020-02-20 19:12:39 +0700

    เพิ่ม Access-Control-Allow-Methods ใน API GATEWAY

--- backend/APIGateway/server.js
+++ backend/APIGateway/server.js
@@ -54,6 +54,7 @@ app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", req.headers.origin);
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
         res.header("Access-Control-Allow-Credentials", true);
+        res.header("Access-Control-Allow-Methods", "*");
     }
     return next();
 })
