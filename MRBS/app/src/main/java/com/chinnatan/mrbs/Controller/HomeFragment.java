1a3461c69f0a2a8b692863bd4a1235a3c91ef531
chinnatan <korn.chinnatan@gmail.com>
2020-02-24 21:28:02 +0700

    เพิ่มระบบแสดงเวลาและวันที่

--- MRBS/app/src/main/java/com/chinnatan/mrbs/Controller/HomeFragment.java
+++ MRBS/app/src/main/java/com/chinnatan/mrbs/Controller/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.chinnatan.mrbs.Controller;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +14,20 @@ import androidx.fragment.app.Fragment;
 import com.chinnatan.mrbs.MainActivity;
 import com.chinnatan.mrbs.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static android.os.Looper.getMainLooper;
+
 public class HomeFragment extends Fragment {
 
     private static final String TAG = "HOME";
+
+    private TextView currentTime;
+    private TextView currentDate;
+
+    private Handler displayTime = new Handler(getMainLooper());
 
     @Nullable
     @Override
@@ -26,5 +39,36 @@ public class HomeFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         MainActivity.onFragmentChanged((TAG));
+
+        init();
+        displayTime();
+    }
+
+    private void init() {
+        currentTime = getView().findViewById(R.id.home_currenttime);
+        currentDate = getView().findViewById(R.id.home_currentdate);
+    }
+
+    private void displayTime() {
+        currentDate.setText(new SimpleDateFormat("dd/MM/YYYY", Locale.US).format(new Date()));
+        displayTime.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                currentTime.setText(new SimpleDateFormat("HH:mm:ss", Locale.US).format(new Date()));
+                displayTime.postDelayed(this, 1000);
+            }
+        }, 10);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        displayTime();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        displayTime = null;
     }
 }
