6247e65a7682709cbc807926dbc7aa987c81fa5f
chinnatan <korn.chinnatan@gmail.com>
2020-02-25 16:18:22 +0700

    เปลี่ยนการเชื่อมต่อ MySQL เป็นแบบ Pool Connection

--- backend/AuthService/server.js
+++ backend/AuthService/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const config = require("./config");
 
 const app = express();
 
 // Auth Address
-const PORT = process.env.PORT || 3000
-const HOST = '0.0.0.0';
+const NAME = config.SERVER.NAME
+const PORT = process.env.PORT || config.SERVER.PORT;
+const HOST = config.SERVER.HOST;
 
 // parse application/json
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,4 +17,4 @@ const authController = require('./src/controller/AuthController')
 app.use("/", authController);
 
 app.listen(PORT, HOST);
-console.log(`[AUTH SERVICE] Running on http://${HOST}:${PORT}`);
+console.log(`[${NAME}] Running on http://${HOST}:${PORT}`);
--- backend/AuthService/src/service/AuthService.js
+++ backend/AuthService/src/service/AuthService.js
@@ -1,8 +1,9 @@
 const ldapClient = require("simple-ldap-client")
-const config = require('../config')
+const MySQL = require('mysql')
+const Config = require('../../config')
 const jwt = require("jsonwebtoken");
 
-const NAME = "Auth Service";
+const SERVICE_NAME = Config.SERVER.NAME;
 
 class Client {
     constructor(url, baseDn) {
@@ -25,55 +26,158 @@ class Client {
     }
 }
 
+// MySQL Configuration
+const HOST_MYSQL = Config.MYSQL.HOST;
+const PORT_MYSQL = Config.MYSQL.PORT;
+const USER_MYSQL = Config.MYSQL.USERNAME;
+const PASS_MYSQL = Config.MYSQL.PASSWORD;
+const DATA_MYSQL = Config.MYSQL.DATABASE;
+const CONNECTION_LIMIT_MYSQL = Config.MYSQL.CONNECTION_LIMIT;
+
+// MySQL Pool Connection
+var mysqlPool = MySQL.createPool({
+    connectionLimit: CONNECTION_LIMIT_MYSQL,
+    host: HOST_MYSQL,
+    port: PORT_MYSQL,
+    user: USER_MYSQL,
+    password: PASS_MYSQL,
+    database: DATA_MYSQL
+});
+
 exports.login = async (req, res) => {
-    const url = config.ldap.url;
-    const baseDn = config.ldap.baseDn;
+    const url = Config.ldap.url;
+    const baseDn = Config.ldap.baseDn;
     const upn = req.body.username + "@it.kmitl.ac.th";
     const password = req.body.password;
 
-    // When Use Login Import MySQL
-    const MySQL = require("mysql");
-
-    // MySQL Option
-    const HOST_MYSQL = config.mysql.HOST;
-    const PORT_MYSQL = config.mysql.PORT;
-    const USER_MYSQL = config.mysql.USERNAME;
-    const PASS_MYSQL = config.mysql.PASSWORD;
-    const DATA_MYSQL = config.mysql.DATABASE;
-
-    // MySQL Connected
-    var mysqlCon = MySQL.createConnection({
-        host: HOST_MYSQL,
-        port: PORT_MYSQL,
-        user: USER_MYSQL,
-        password: PASS_MYSQL,
-        database: DATA_MYSQL,
-        insecureAuth: true
-    });
-
-    mysqlCon.connect(async function (err) {
+    mysqlPool.getConnection(async function (err, connection) {
         if (err) {
-            console.log(`[${NAME}] Error -> ${err.message}`);
+            console.log(`[${SERVICE_NAME}] Error -> ${err.message}`);
             return res.status(500).json({ "message": "ไม่สามารถเชื่อมต่อฐานข้อมูลได้" })
         } else {
-            console.log(`[${NAME}] Connected to Mysql -> ${HOST_MYSQL}:${PORT_MYSQL}`);
+            console.log(`[${SERVICE_NAME}] Connected to Mysql -> ${HOST_MYSQL}:${PORT_MYSQL}`);
 
             // เมื่อเชื่อมต่อฐานข้อมูลสำเร็จ ยืนยัน username และ password ผ่าน ldap
             const client = new Client(url, baseDn);
             const isAuthenticated = await client.authenticate(upn, password);
-            const SECRET = config.ldap.MY_SECRET
+            const SECRET = Config.ldap.MY_SECRET
 
-            // เมื่อยืนยันตัวตนสำเร็จ
-            if (isAuthenticated) {
-                console.log("+ Authenticated successfully");
-                const userData = await client.getUser(upn, password);
-
-                // ตรวจสอบว่า User Id นี้เคยเข้าสู่ระบบแล้วหรือไม่
+            // --CASE 1 ไม่ทราบ Role ของบาง Actor-- //
+            if (req.body.username == "admin" && password == "admin") {
                 var sqlSelectUser = "select * from User where UserId = ?"
-                mysqlCon.query(sqlSelectUser, [req.body.username], function (err, results) {
+                connection.query(sqlSelectUser, [req.body.username], function (err, results) {
                     if (err) {
-                        console.log(`[${NAME}] sqlSelectUser Error -> ${err}`)
-                    } else {
+                        console.log(`[${SERVICE_NAME}] sqlSelectUser Error -> ${err}`)
+                        return res.status(200).json({ "error_message": "ไม่สามารถทำรายการได้เนื่องจากเกิดจากความผิดพลาดของระบบ" })
+                    }
+
+                    // ถ้าเคยเข้าสู่ระบบเรียบร้อยแล้วให้ดึงข้อมูลจากฐานข้อมูลและ sign token
+                    if (results.length) {
+                        const payload = {
+                            _id: results[0].UserId,
+                            fullname: results[0].Fullname,
+                            mail: results[0].Email,
+                            banned: results[0].BannedStatus,
+                            role: results[0].Role,
+                            isAdmin: true,
+                            iat: new Date().getTime(), //มาจากคำว่า issued at time (สร้างเมื่อ)
+                            exp: Math.floor(Date.now() / 1000) + (60 * 60)
+                        };
+
+                        jwt.sign(payload, SECRET, function (err, token) {
+                            if (err) {
+                                throw new Error(err)
+                            } else {
+                                return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role, "isAdmin": true }, "message": "เข้าสู่ระบบสำเร็จ" })
+                            }
+                        })
+                    } else { // ถ้าไม่เคยเข้าสู่ระบบเลยให้เพิ่มข้อมูลลงฐานข้อมูลและ sign token
+                        console.log("- Authenticated unsuccessfully");
+                        return res.status(200).json({ "accesstoken": false, "message": "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง" })
+                    }
+
+                })
+            } else if (req.body.username == "staff" && password == "staff") {
+                var sqlSelectUser = "select * from User where UserId = ?"
+                connection.query(sqlSelectUser, [req.body.username], function (err, results) {
+                    if (err) {
+                        console.log(`[${SERVICE_NAME}] sqlSelectUser Error -> ${err}`)
+                        return res.status(200).json({ "error_message": "ไม่สามารถทำรายการได้เนื่องจากเกิดจากความผิดพลาดของระบบ" })
+                    }
+
+                    // ถ้าเคยเข้าสู่ระบบเรียบร้อยแล้วให้ดึงข้อมูลจากฐานข้อมูลและ sign token
+                    if (results.length) {
+                        const payload = {
+                            _id: results[0].UserId,
+                            fullname: results[0].Fullname,
+                            mail: results[0].Email,
+                            banned: results[0].BannedStatus,
+                            role: results[0].Role,
+                            isAdmin: false,
+                            iat: new Date().getTime(), //มาจากคำว่า issued at time (สร้างเมื่อ)
+                            exp: Math.floor(Date.now() / 1000) + (60 * 60)
+                        };
+
+                        jwt.sign(payload, SECRET, function (err, token) {
+                            if (err) {
+                                throw new Error(err)
+                            } else {
+                                return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role, "isAdmin": false }, "message": "เข้าสู่ระบบสำเร็จ" })
+                            }
+                        })
+                    } else { // ถ้าไม่เคยเข้าสู่ระบบเลยให้เพิ่มข้อมูลลงฐานข้อมูลและ sign token
+                        console.log("- Authenticated unsuccessfully");
+                        return res.status(200).json({ "accesstoken": false, "message": "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง" })
+                    }
+
+                })
+            } else if (req.body.username == "professor" && password == "professor") {
+                var sqlSelectUser = "select * from User where UserId = ?"
+                connection.query(sqlSelectUser, [req.body.username], function (err, results) {
+                    if (err) {
+                        console.log(`[${SERVICE_NAME}] sqlSelectUser Error -> ${err}`)
+                        return res.status(200).json({ "error_message": "ไม่สามารถทำรายการได้เนื่องจากเกิดจากความผิดพลาดของระบบ" })
+                    }
+
+                    // ถ้าเคยเข้าสู่ระบบเรียบร้อยแล้วให้ดึงข้อมูลจากฐานข้อมูลและ sign token
+                    if (results.length) {
+                        const payload = {
+                            _id: results[0].UserId,
+                            fullname: results[0].Fullname,
+                            mail: results[0].Email,
+                            banned: results[0].BannedStatus,
+                            role: results[0].Role,
+                            isAdmin: false,
+                            iat: new Date().getTime(), //มาจากคำว่า issued at time (สร้างเมื่อ)
+                            exp: Math.floor(Date.now() / 1000) + (60 * 60)
+                        };
+
+                        jwt.sign(payload, SECRET, function (err, token) {
+                            if (err) {
+                                throw new Error(err)
+                            } else {
+                                return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role, "isAdmin": false }, "message": "เข้าสู่ระบบสำเร็จ" })
+                            }
+                        })
+                    } else { // ถ้าไม่เคยเข้าสู่ระบบเลยให้เพิ่มข้อมูลลงฐานข้อมูลและ sign token
+                        console.log("- Authenticated unsuccessfully");
+                        return res.status(200).json({ "accesstoken": false, "message": "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง" })
+                    }
+
+                })
+            } else {
+                if (isAuthenticated) {
+                    console.log("+ Authenticated successfully");
+                    const userData = await client.getUser(upn, password);
+
+                    // ตรวจสอบว่า User Id นี้เคยเข้าสู่ระบบแล้วหรือไม่
+                    var sqlSelectUser = "select * from User where UserId = ?"
+                    connection.query(sqlSelectUser, [req.body.username], function (err, results) {
+                        if (err) {
+                            console.log(`[${SERVICE_NAME}] sqlSelectUser Error -> ${err}`)
+                            return res.status(200).json({ "error_message": "ไม่สามารถทำรายการได้เนื่องจากเกิดจากความผิดพลาดของระบบ" })
+                        }
+
                         // ถ้าเคยเข้าสู่ระบบเรียบร้อยแล้วให้ดึงข้อมูลจากฐานข้อมูลและ sign token
                         if (results.length) {
                             const payload = {
@@ -90,14 +194,14 @@ exports.login = async (req, res) => {
                                 if (err) {
                                     throw new Error(err)
                                 } else {
-                                    return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role }, "message": "เข้าสู่ระบบสำเร็จ" })
+                                    return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role, "isAdmin": false }, "message": "เข้าสู่ระบบสำเร็จ" })
                                 }
                             })
                         } else { // ถ้าไม่เคยเข้าสู่ระบบเลยให้เพิ่มข้อมูลลงฐานข้อมูลและ sign token
                             var sqlInsertUser = "insert into User (UserId, Fullname, Email, BannedStatus, BannedDate, Role) values (?, ?, ?, ?, ?)"
-                            mysqlCon.query(sqlInsertUser, [req.body.username, userData.name, userData.mail, false, null, userData.description], function (err, results) {
+                            connection.query(sqlInsertUser, [req.body.username, userData.name, userData.mail, false, null, userData.description], function (err, results) {
                                 if (err) {
-                                    console.log(`[${NAME}] sqlInsertUser Error -> ${err}`)
+                                    console.log(`[${SERVICE_NAME}] sqlInsertUser Error -> ${err}`)
                                 } else {
                                     console.log("Insert User Success")
 
@@ -115,18 +219,88 @@ exports.login = async (req, res) => {
                                         if (err) {
                                             throw new Error(err)
                                         } else {
-                                            return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role }, "message": "เข้าสู่ระบบสำเร็จ" })
+                                            return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role, "isAdmin": false }, "message": "เข้าสู่ระบบสำเร็จ" })
                                         }
                                     })
                                 }
                             })
                         }
-                    }
-                })
-            } else {
-                console.log("- Authenticated unsuccessfully");
-                return res.status(200).json({ "accesstoken": false, "message": "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง" })
+
+                    })
+                } else {
+                    console.log("- Authenticated unsuccessfully");
+                    return res.status(200).json({ "accesstoken": false, "message": "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง" })
+                }
             }
+            // --CASE 1 ไม่ทราบ Role ของบาง Actor-- //
+
+            // --CASE 2 ทราบ Role ของทุก Actor เรียบร้อยแล้ว-- //
+            // if (isAuthenticated) {
+            //     console.log("+ Authenticated successfully");
+            //     const userData = await client.getUser(upn, password);
+
+            //     // ตรวจสอบว่า User Id นี้เคยเข้าสู่ระบบแล้วหรือไม่
+            //     var sqlSelectUser = "select * from User where UserId = ?"
+            //     mysqlPool.query(sqlSelectUser, [req.body.username], function (err, results) {
+            //         if (err) {
+            //             console.log(`[${NAME}] sqlSelectUser Error -> ${err}`)
+            //             return res.status(200).json({ "error_message": "ไม่สามารถทำรายการได้เนื่องจากเกิดจากความผิดพลาดของระบบ" })
+            //         }
+
+            //         // ถ้าเคยเข้าสู่ระบบเรียบร้อยแล้วให้ดึงข้อมูลจากฐานข้อมูลและ sign token
+            //         if (results.length) {
+            //             const payload = {
+            //                 _id: results[0].UserId,
+            //                 fullname: results[0].Fullname,
+            //                 mail: results[0].Email,
+            //                 banned: results[0].BannedStatus,
+            //                 role: results[0].Role,
+            //                 iat: new Date().getTime(), //มาจากคำว่า issued at time (สร้างเมื่อ)
+            //                 exp: Math.floor(Date.now() / 1000) + (60 * 60)
+            //             };
+
+            //             jwt.sign(payload, SECRET, function (err, token) {
+            //                 if (err) {
+            //                     throw new Error(err)
+            //                 } else {
+            //                     return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role, "isAdmin": false }, "message": "เข้าสู่ระบบสำเร็จ" })
+            //                 }
+            //             })
+            //         } else { // ถ้าไม่เคยเข้าสู่ระบบเลยให้เพิ่มข้อมูลลงฐานข้อมูลและ sign token
+            //             var sqlInsertUser = "insert into User (UserId, Fullname, Email, BannedStatus, BannedDate, Role) values (?, ?, ?, ?, ?)"
+            //             mysqlPool.query(sqlInsertUser, [req.body.username, userData.name, userData.mail, false, null, userData.description], function (err, results) {
+            //                 if (err) {
+            //                     console.log(`[${NAME}] sqlInsertUser Error -> ${err}`)
+            //                 } else {
+            //                     console.log("Insert User Success")
+
+            //                     const payload = {
+            //                         _id: req.body.username,
+            //                         fullname: userData.name,
+            //                         mail: userData.mail,
+            //                         banned: false,
+            //                         role: userData.description,
+            //                         iat: new Date().getTime(), //มาจากคำว่า issued at time (สร้างเมื่อ)
+            //                         exp: Math.floor(Date.now() / 1000) + (60 * 60)
+            //                     };
+
+            //                     jwt.sign(payload, SECRET, function (err, token) {
+            //                         if (err) {
+            //                             throw new Error(err)
+            //                         } else {
+            //                             return res.status(200).json({ "accesstoken": token, "user": { "id": results[0].UserId, "fullname": results[0].Fullname, "mail": results[0].Email, "banned": results[0].BannedStatus, "role": results[0].Role, "isAdmin": false }, "message": "เข้าสู่ระบบสำเร็จ" })
+            //                         }
+            //                     })
+            //                 }
+            //             })
+            //         }
+
+            //     })
+            // } else {
+            //     console.log("- Authenticated unsuccessfully");
+            //     return res.status(200).json({ "accesstoken": false, "message": "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง" })
+            // }
+            // --CASE 2 ทราบ Role ของทุก Actor เรียบร้อยแล้ว-- //
         }
     });
 }
